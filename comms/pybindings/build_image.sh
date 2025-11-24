3ab5c6f382ba86d0233bcff879430b9945db4f62
Aadithyaa Eeswaran <eaadithyaa@gmail.com>
2025-11-24 13:59:02 +0530

    Ft: dummy test img

--- comms/pybindings/build_image.sh
+++ comms/pybindings/build_image.sh
@@ -3,3 +3,4 @@ DOCKER_CMD="${DOCKER_CMD:-podman}"
 rm grpc_test_img.tar
 $DOCKER_CMD build -t volpe_grpc_test .
 $DOCKER_CMD save -o grpc_test_img.tar volpe_grpc_test
+cp grpc_test_img.tar ../../cli/img.tar
