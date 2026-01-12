eac34d8fe207361d8986f762abe613487aae2a42
Aadithyaa Eeswaran <eaadithyaa@gmail.com>
2026-01-12 10:29:08 +0530

    Refactor: moved pybindings to volpe-py

--- comms/pybindings_bak/build_image.sh
+++
@@ -1,6 +0,0 @@
-DOCKER_CMD="${DOCKER_CMD:-podman}"
-
-rm grpc_test_img.tar
-$DOCKER_CMD build -t volpe_grpc_test .
-$DOCKER_CMD save -o grpc_test_img.tar volpe_grpc_test
-cp grpc_test_img.tar ../../cli/img.tar
